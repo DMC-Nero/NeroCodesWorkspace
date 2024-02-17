a41f56c8476ec35ee786c30eb5bb89bd1dd03b93
DMC-Nero <vemulanatraj@gmail.com>
2024-02-17 19:39:31 +0530

    lilkl

---
+++ BroCodes/src/bro/Array.java
@@ -0,0 +1,13 @@
+package bro;
+
+public class Array {
+	public static void main(String[] args) {
+		String[] cars= {"Cammaro","zen","bugatti"};
+		//cars[0]="tesla";
+		for(int i=0;i<cars.length;i++) {
+			System.out.println(cars[i]);
+		}
+		
+	}
+
+}
---
+++ BroCodes/src/bro/GUI_Conversation.java
@@ -0,0 +1,17 @@
+package bro;
+import javax.swing.JOptionPane;
+public class GUI_Conversation {
+	public static void main(String[] args) {
+		String name=JOptionPane.showInputDialog("what is your name....?");
+		JOptionPane.showMessageDialog(null,"Hi "+ name +"!!!!!!");
+		
+		int age=Integer.parseInt(JOptionPane.showInputDialog("what is your age....?"));
+		JOptionPane.showMessageDialog(null,"soooo you are "+age +" years old !!!!!!");
+		
+		double height=Double.parseDouble(JOptionPane.showInputDialog("your Height"));
+		JOptionPane.showMessageDialog(null, "Hmmmm you are "+ height + " feets tall !!!!!!");
+		
+		
+	}
+
+}
