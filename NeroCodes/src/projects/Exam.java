002fa1b3fdb82c17e1d4348ad8b13fd9f526f53f
DMC-Nero <vemulanatraj@gmail.com>
2024-02-21 12:54:49 +0530

    exam

--- NeroCodes/src/projects/Exam.java
+++ NeroCodes/src/projects/Exam.java
@@ -1,5 +1,16 @@
 package projects;
 
 public class Exam {
+	public static void main(String[] args) {
+		System.out.println("main start");
+		Sam();
+		System.out.println("main end");
+	}
+	
+	public static void Sam(){
+		int x=10;
+		System.out.println("value of x : "+x);
+		x=x+5;
+	}
 	
 }
