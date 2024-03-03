584abad5cfab2ee03e77bf25106fddad2d912711
DMC-Nero <vemulanatraj0123@gmail.com>
2024-03-03 17:53:59 +0530

    factoryMethod

--- NeroCodesQedge/src/qedge_class/FactoryMethod.java
+++ NeroCodesQedge/src/qedge_class/FactoryMethod.java
@@ -4,16 +4,16 @@ public class FactoryMethod {
 
 	public static void main(String[] args) {
 
-		FactoryMethod_demo2 st1 = FactoryMethod_demo1.creatStudentWithData(123, "jack");		//assigning values to object st1 of demo type
+		FactDemo2 st1 = FactDemo1.creatStudentWithData(123, "jack");							//assigning values to object st1 of demo type
 		st1.printStudentInfo();																	//invoking method printStudentInfo
 
-		FactoryMethod_demo2 st2 = FactoryMethod_demo1.creatStudentWithData(234, "Nero");		//assigning values to object st2 of demo type
+		FactDemo2 st2 = FactDemo1.creatStudentWithData(234, "Nero");							//assigning values to object st2 of demo type
 		st2.printStudentInfo();																	//invoking method printStudentInfo
 	}
 
 }
 
-class FactoryMethod_demo2 {																		//class to creating object of demo type
+class FactDemo2 {																				//class to creating object of demo type
 	int sid;																					//instance variable 
 	String sname;																				//instance variable
 
@@ -22,9 +22,9 @@ class FactoryMethod_demo2 {																		//class to creating object of demo 
 	}
 }
 
-class FactoryMethod_demo1 {																		//new class for assigning address to pointer
-	public static FactoryMethod_demo2 creatStudentWithData(int sidInput, String snameInput) {	//static method with return value and parameters sid and sname
-		FactoryMethod_demo2 s1 = new FactoryMethod_demo2();										//creating object s1 of demo type
+class FactDemo1 {																				//new class for assigning address to pointer
+	public static FactDemo2 creatStudentWithData(int sidInput, String snameInput) {				//static method with return value and parameters sid and sname
+		FactDemo2 s1 = new FactDemo2();															//creating object s1 of demo type
 		s1.sid = sidInput;																		//assigning sidInput to s1.sid
 		s1.sname = snameInput;																	//assigning snameInput to s1.sname
 		return s1;																				//returning s1 object
