21f3d8d0fda57ad37ea346e280e4cc6d3a7d73ec
DMC-Nero <vemulanatraj@gmail.com>
2024-03-09 18:32:15 +0530

    Static and non-static blocks

---
+++ NeroCodesQedge/src/qedge_class/StaticNonStatic_Block.java
@@ -0,0 +1,41 @@
+package qedge_class;
+
+public class StaticNonStatic_Block {
+
+	static {
+		System.out.println("in static block of Exam class");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("main start");
+		
+		BlockStudent s1 = new BlockStudent();
+		
+		BlockStudent s2 = new BlockStudent(2,"jane");
+		
+		System.out.println("main end");
+	}
+}
+
+class BlockStudent{
+	int sid;
+	String sname;
+	static {
+		System.out.println("in static block of BlockStudent");
+	}
+	
+	{
+		System.out.println("in non-static block of BlockStudent");
+	}
+	
+	public BlockStudent() {
+		System.out.println("in BlockStudent() constructor");
+		sid = 1;
+		sname = "jack";
+	}
+	
+	public BlockStudent(int id,String name) {
+		sid = id;
+		sname = name;
+	}
+}
