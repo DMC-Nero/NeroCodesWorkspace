629bd4b11ecd097517bf17b6ee566d1f3a46ce6c
DMC-Nero <vemulanatraj0123@gmail.com>
2024-05-19 13:20:01 +0530

    ArrayListCollection

---
+++ java_collection/collectionsTYguide/org/java/ArrayList/C005_ArrayListDemo.java
@@ -0,0 +1,36 @@
+package org.java.ArrayList;
+
+/*
+	 1. An ArrayList is re-sizable array, also called a dynamic array. It grows its size to 
+		accommodate new elements and shrinks the size when the elements are removed.
+	 2. ArrayList internally uses an array to store the elements. Just like arrays, it allows 
+	 	you to retrieve the elements by their index.
+	 3. Java ArrayList allows duplicate and null values.
+	 4. Java ArrayList is an ordered collection. It maintains the insertion order of the 
+	 	elements.
+	 5. You cannot create an ArrayList of primitive types like int, char etc. You need to
+	 	use boxed types like Integer, Character, Boolean etc.
+	 
+  	 Note:-	 Default capacity of an ArrayList is [10].
+ */
+
+public class C005_ArrayListDemo {
+
+	public static void main(String[] args) {
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
