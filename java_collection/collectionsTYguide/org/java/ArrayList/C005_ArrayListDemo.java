079e691b05f61255e11991767bc35f84ed624c36
DMC-Nero <vemulanatraj0123@gmail.com>
2024-05-20 00:37:10 +0530

    ArrayListDemo

--- java_collection/collectionsTYguide/org/java/ArrayList/C005_ArrayListDemo.java
+++ java_collection/collectionsTYguide/org/java/ArrayList/C005_ArrayListDemo.java
@@ -1,5 +1,8 @@
 package org.java.ArrayList;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 	 1. An ArrayList is re-sizable array, also called a dynamic array. It grows its size to 
 		accommodate new elements and shrinks the size when the elements are removed.
@@ -10,14 +13,24 @@ package org.java.ArrayList;
 	 	elements.
 	 5. You cannot create an ArrayList of primitive types like int, char etc. You need to
 	 	use boxed types like Integer, Character, Boolean etc.
-	 
-  	 Note:-	 Default capacity of an ArrayList is [10].
+	 6. Java ArrayList is not synchronized. If multiple threads try to modify an ArrayList at
+	 	the same time, then the final outcome will be non-deterministic. You must explicitly 
+	 	synchronize access to an ArrayList if multiple threads are gonna modify it.
+	 	
+  				--> Note:-	 Default capacity of an ArrayList is [10]. <--
  */
 
 public class C005_ArrayListDemo {
 
 	public static void main(String[] args) {
-
+	// creating an ArrayList object
+		List<String> fruits = new ArrayList<>();
+		fruits.add("Banana");
+		fruits.add("Mango");
+		fruits.add("Apple");
+		fruits.add("Orange");
+		System.out.println(fruits);
+		 
 	}
 
 }
