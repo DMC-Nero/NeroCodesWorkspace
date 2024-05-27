179c242244ec7adeb9da1aaec644141447fbc05d
DMC-Nero <vemulanatraj0123@gmail.com>
2024-05-28 01:11:53 +0530

    11:11

---
+++ java_collection/collectionsTYguide/org/java/HashMap/C022_Student.java
@@ -0,0 +1,19 @@
+package org.java.HashMap;
+
+public class C022_Student {
+
+	private String firstName;
+	private String lastName;
+	
+	public C022_Student(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		return "C022_Student [firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+	
+}
