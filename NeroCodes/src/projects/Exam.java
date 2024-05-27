c8948bd3ec33d756694ee018e0d903065d9e115e
DMC-Nero <vemulanatraj0123@gmail.com>
2024-05-28 01:10:28 +0530

    11:11

--- NeroCodes/src/projects/Exam.java
+++ NeroCodes/src/projects/Exam.java
@@ -1,16 +1,33 @@
 package projects;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 public class Exam {
-	public static void main(String[] args) {
-		Sample s1 = new Sample();
-		System.out.println(s1);
-		s1.fun1();
+	public static void main(String[] args) throws IOException {
+		
+		String data = "java world";
+		byte[] barr = data.getBytes();
+		
+		// ByteArrayInputStream
+		ByteArrayInputStream bais = new ByteArrayInputStream(barr);
+		
+		int byteRead = 0;
+		while( (byteRead = bais.read()) != -1) {
+			System.out.print((char) byteRead);
+		}
+		//////////////////
+		System.out.println();
+		
+		// ByteArrayOutputStream 
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		baos.write(97);
+		baos.write(98);
+		baos.write(99);
+		baos.write(100);
+		
+		System.out.println(baos.toString());
 	}
 }
 
-class Sample{
-	public void fun1() {
-		System.out.println("fun() invoked");
-		System.out.println(this);
-	}
-}
