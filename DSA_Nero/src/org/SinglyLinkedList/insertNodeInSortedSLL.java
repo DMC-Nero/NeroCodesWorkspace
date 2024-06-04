deb75be5346de33685946d7fb1baf7a42ce6c62d
DMC-Nero <vemulanatraj0123@gmail.com>
2024-06-04 17:58:53 +0530

    insertNodeInSortedSLL

---
+++ DSA_Nero/src/org/SinglyLinkedList/insertNodeInSortedSLL.java
@@ -0,0 +1,28 @@
+package org.SinglyLinkedList;
+
+public class insertNodeInSortedSLL {
+	
+	private ListNode head;
+	
+	private class ListNode {
+		int data;
+		ListNode next;
+		
+		public ListNode(int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
+	// --------------------------------------------------------------------------------------------
+	public void display() {
+		
+	}
+	
+	
+	
+	// --------------------------------------------------------------------------------------------
+	public static void main(String[] args) {
+		
+	}
+
+}
